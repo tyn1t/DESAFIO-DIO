f574955115bc900d0727080763101dfe2b2096e1
tyn1t <anderlansouza71@gmail.com>
2022-04-16 08:27:43 -0400

    Desafio dio projeito netflix

---
+++ js/owl/setup.js
@@ -0,0 +1,28 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    nav:false,
+    margin:10,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },            
+        960:{
+            items:5
+        },
+        1200:{
+            items:6
+        }
+    }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
